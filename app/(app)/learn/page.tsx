78e78a8f5874d00a3da8d676fd5b6628af8f345f
volodymyrShcherbatyy <58917704+volodymyrShcherbatyy@users.noreply.github.com>
2026-02-17 14:55:58 +0100

    Remove description prop from ImageWordCard
    Remove description from ImageWordCard component.

--- app/(app)/learn/page.tsx
+++ app/(app)/learn/page.tsx
@@ -15,7 +15,7 @@ export default async function LearnPage() {
             <ImageWordCard
               text={word.text}
               imageUrl={word.imageUrl}
-              description={`${word.text}`}
+              description={`${}`}
               translation={word.translationsFrom[0]?.translatedWord.text ?? "-"}
             />
           </div>
