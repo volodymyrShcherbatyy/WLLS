8ff08a62e2885f7dbd3a82b483c10cc6f441ba5c
volodymyrShcherbatyy <58917704+volodymyrShcherbatyy@users.noreply.github.com>
2026-02-17 14:46:23 +0100

    Add description prop to ImageWordCard component

--- app/(app)/learn/page.tsx
+++ app/(app)/learn/page.tsx
@@ -15,6 +15,7 @@ export default async function LearnPage() {
             <ImageWordCard
               text={word.text}
               imageUrl={word.imageUrl}
+              description="description"
               translation={word.translationsFrom[0]?.translatedWord.text ?? "-"}
             />
           </div>
