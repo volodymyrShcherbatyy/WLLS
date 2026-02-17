159bbe614c3dc3ac905640f3ab173428c21ddcbe
volodymyrShcherbatyy <58917704+volodymyrShcherbatyy@users.noreply.github.com>
2026-02-17 14:45:20 +0100

    Remove description prop from ImageWordCard
    Removed hardcoded description prop from ImageWordCard.

--- app/(app)/learn/page.tsx
+++ app/(app)/learn/page.tsx
@@ -15,7 +15,6 @@ export default async function LearnPage() {
             <ImageWordCard
               text={word.text}
               imageUrl={word.imageUrl}
-              description="description"
               translation={word.translationsFrom[0]?.translatedWord.text ?? "-"}
             />
           </div>
